2cd0a5cc5b731947af02ca30bc81ed7571a8e474
HT-CHEN520 <252593241@qq.com>
2022-01-12 12:44:11 +0800

    feat: post-table

---
+++ db/schema/000008_post_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `sys_post` (
+  `post_id` varchar(255) PRIMARY KEY NOT NULL,
+  `post_name` varchar(128) DEFAULT NULL,
+  `post_code` varchar(128) DEFAULT NULL unique,
+  `sort` int DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `create_by` varchar(128) DEFAULT NULL,
+  `update_by` varchar(128) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT '2000-01-01 00:00:00',
+  `delete_time` timestamp NULL DEFAULT '2000-01-01 00:00:00'
+);
